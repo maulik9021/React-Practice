6c69e9501e4ff01507ceb5ddbe9e157a1ca52b02
Maulik Patel <patelmaulik212@gmail.com>
2024-01-02 15:00:37 -0500

    Igniting our app

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React Practice 🚀
+
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Content Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Error Handling
+- Diagnostics
+- HTTPs
+- Tree Shaking - remove unused code
--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const heading = React.createElement(
-   "h1", 
-   { id : "heading"}, 
-   "Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parent = React.createElement("div", { id : "parent" }, [
+   React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "This is h1 tag"),
+      React.createElement("h1", {}, "This is develop by Maulik"),
+   ])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);

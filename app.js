ec2834003dc140da811d40186d8b8bf900a9c7b9
Maulik Patel <patelmaulik212@gmail.com>
2024-01-08 15:39:24 -0500

    episode-3

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-import React from "react";
-import ReactDOM from "react-dom";
+/* import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id : "parent" }, [
-   React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "This is h1 tag"),
-      React.createElement("h1", {}, "This is develop by Maulik"),
-   ])
-]);
+const headerElement = React.createElement(
+   "div",
+   {className:"title"},
+   [
+      React.createElement("h1", {}, "Heading tag 1"),
+      React.createElement("h2", {}, "Heading tag 2"),
+      React.createElement("h3", {}, "Heading tag 3"),
+   ]
+);
+
+const headerJSX = (
+   <div className="title">
+      <h1>JSX Heading Tag 1</h1>
+      <h2>JSX Heading Tag 2</h2>
+      <h3>JSX Heading Tag 3</h3>
+   </div>
+);
+
+
+const TitleComponent = () => (
+   <div>
+      <h1>Title Heading 1</h1>
+      <h2>Title Heading 2</h2>
+      <h3>Title Heading 3</h3>
+   </div>
+);
+
+
+const HeadingComponent = () => (
+   <div id="container">
+      {headerElement}
+      {headerJSX}
+      {TitleComponent()}
+   </div> 
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent/>);
+
+
+/* const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(headerElement);
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+
+root2.render(headerJSX); */ 
---
+++ p1.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from './images/react-logo.png';
+import { FaUser } from "react-icons/fa";
+
+const LogoHead = () => (
+   <div id="logo_head">
+      <img src={logo} alt="logo" width={80} height={80}/>
+
+      <div className="searchbar">
+         <input type="text" className="text-search" />
+         <button className="btn-search">Search</button>
+      </div>
+
+      <FaUser id="fa-user"/>
+   </div>
+);
+
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+
+root2.render(<LogoHead/>);
